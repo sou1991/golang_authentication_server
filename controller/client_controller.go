1190636c2136abb8371a36aca4d10709c9a2bf29
sou_devlop <sou.business0109@gmail.com>
2023-11-14 23:09:14 +0900

    feat auth client checker

---
+++ controller/client_controller.go
@@ -0,0 +1,22 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sou1991/golang_authentication_server/repository"
+)
+
+type ClientChecker interface{
+	CheckClent(...*gin.Context)
+}
+
+type ClientController struct{
+	Cr repository.ClientChecker
+}
+
+func NewClentController(cr repository.ClientChecker) ClientChecker{
+	return ClientController{Cr: cr}
+}
+
+func (cc ClientController) CheckClent(c ...*gin.Context){
+	cc.Cr.CheckClent(c[0])
+}
---
+++ repository/client_repository.go
@@ -0,0 +1,20 @@
+package repository
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sou1991/golang_authentication_server/entity"
+)
+
+type ClientRepository struct{}
+
+func NewClientRepository() ClientChecker{
+	return ClientRepository{}
+}
+
+type ClientChecker interface{
+	CheckClent(...*gin.Context)
+}
+
+func (ClientRepository) CheckClent(c ...*gin.Context){
+	entity.CheckAvailableClients(c[0])
+}
